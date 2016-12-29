bfe6272885fce8f39bf43dffbd27479c2c12a3c1
Jake Dexheimer <jakedex@gmail.com>
2016-12-28 23:06:05 -0600

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/main',
+    'webpack-dev-server/client?http://localhost:8080'
+  ],
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './src'
+  }
+}
